6ad1169da473bc488ce4d400a32f8d59b0616856
TECHNICALREVA <revanth.webs.apps@gmail.com>
2021-08-19 18:06:45 +0530

    technicalreva

---
+++ app/src/main/java/app/tech/reva/MainActivity.java
@@ -0,0 +1,285 @@
+package app.tech.reva;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import com.google.android.material.appbar.AppBarLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import android.widget.LinearLayout;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebViewClient;
+import android.view.View;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+
+
+public class MainActivity extends  AppCompatActivity  { 
+	
+	
+	private Toolbar _toolbar;
+	private AppBarLayout _app_bar;
+	private CoordinatorLayout _coordinator;
+	private DrawerLayout _drawer;
+	
+	private LinearLayout linear1;
+	private WebView webview1;
+	private LinearLayout _drawer_linear1;
+	private LinearLayout _drawer_linear2;
+	private LinearLayout _drawer_linear3;
+	private LinearLayout _drawer_linear4;
+	private Button _drawer_button1;
+	private ImageView _drawer_imageview1;
+	private Button _drawer_button2;
+	private ImageView _drawer_imageview2;
+	private Button _drawer_button3;
+	
+	private Intent i = new Intent();
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		_app_bar = (AppBarLayout) findViewById(R.id._app_bar);
+		_coordinator = (CoordinatorLayout) findViewById(R.id._coordinator);
+		_toolbar = (Toolbar) findViewById(R.id._toolbar);
+		setSupportActionBar(_toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _v) {
+				onBackPressed();
+			}
+		});
+		_drawer = (DrawerLayout) findViewById(R.id._drawer);
+		ActionBarDrawerToggle _toggle = new ActionBarDrawerToggle(MainActivity.this, _drawer, _toolbar, R.string.app_name, R.string.app_name);
+		_drawer.addDrawerListener(_toggle);
+		_toggle.syncState();
+		
+		LinearLayout _nav_view = (LinearLayout) findViewById(R.id._nav_view);
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		webview1 = (WebView) findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		_drawer_linear1 = (LinearLayout) _nav_view.findViewById(R.id.linear1);
+		_drawer_linear2 = (LinearLayout) _nav_view.findViewById(R.id.linear2);
+		_drawer_linear3 = (LinearLayout) _nav_view.findViewById(R.id.linear3);
+		_drawer_linear4 = (LinearLayout) _nav_view.findViewById(R.id.linear4);
+		_drawer_button1 = (Button) _nav_view.findViewById(R.id.button1);
+		_drawer_imageview1 = (ImageView) _nav_view.findViewById(R.id.imageview1);
+		_drawer_button2 = (Button) _nav_view.findViewById(R.id.button2);
+		_drawer_imageview2 = (ImageView) _nav_view.findViewById(R.id.imageview2);
+		_drawer_button3 = (Button) _nav_view.findViewById(R.id.button3);
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				SketchwareUtil.showMessage(getApplicationContext(), "TECHNICALREVA");
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		_drawer_button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				SketchwareUtil.showMessage(getApplicationContext(), "COMING SOON!");
+			}
+		});
+		
+		_drawer_button2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				i.setClass(getApplicationContext(), WebActivity.class);
+				startActivity(i);
+			}
+		});
+		
+		_drawer_button3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				shareApplication();
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		i.setClass(getApplicationContext(), SplashActivity.class);
+		startActivity(i);
+		webview1.loadUrl("https://youtube.com/channel/UCEe1ARrhMzUcB3Q_e1JGBcw");
+		StrictMode.VmPolicy.Builder builder = 
+						new StrictMode.VmPolicy.Builder(); 
+						StrictMode.setVmPolicy(builder.build());
+						if(Build.VERSION.SDK_INT>=24){ 
+								    try{
+												java.lang.reflect.Method m = 
+										        StrictMode.class.getMethod(
+												"disableDeathOnFileUriExposure"); 
+										        m.invoke(null); 
+										}
+									catch(Exception e){ 
+												showMessage(e.toString()); 
+										} 
+						}
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if (_drawer.isDrawerOpen(GravityCompat.START)) {
+			_drawer.closeDrawer(GravityCompat.START);
+		}
+		else {
+			super.onBackPressed();
+		}
+	}
+	public void _share () {
+	}
+	private void shareApplication() { 
+								android.content.pm.ApplicationInfo app = 
+								getApplicationContext().getApplicationInfo(); 
+								String filePath = app.sourceDir;
+								Intent intent = new Intent(Intent.ACTION_SEND); 
+								intent.setType("*/*"); 
+								java.io.File originalApk = new java.io.File(filePath); 
+								try {  
+											java.io.File tempFile = new java.io.File(getExternalCacheDir() + "/ExtractedApk"); 
+									 		if (!tempFile.isDirectory()) 
+												if (!tempFile.mkdirs()) 
+												return; 
+												tempFile = new java.io.File(tempFile.getPath() + "/" + 
+												"TechnicalReva.apk");
+												if (!tempFile.exists()) 
+													{
+															try{
+																		if (!tempFile.createNewFile()) { 
+																					return; }
+																}
+															catch (java.io.IOException e){} 
+													} 
+												java.io.InputStream in = new java.io.FileInputStream (originalApk);
+												java.io.OutputStream out = new java.io.FileOutputStream(tempFile);
+												byte[] buf = new byte[1024];
+												int len; 
+												while ((len = in.read(buf)) > 0) { 
+																out.write(buf, 0, len); 
+												} 
+												in.close(); 
+												out.close(); 
+												intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(tempFile));
+												startActivity(Intent.createChooser(intent, "Share app via"));
+								} 
+								catch (java.io.IOException e) 
+								{ showMessage(e.toString()); 
+								} 
+				}
+	{
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
---
+++ app/src/main/java/app/tech/reva/WebActivity.java
@@ -0,0 +1,188 @@
+package app.tech.reva;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.LinearLayout;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+
+public class WebActivity extends  AppCompatActivity  { 
+	
+	
+	private LinearLayout linear1;
+	private WebView webview1;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.web);
+		initialize(_savedInstanceState);
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+		|| ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+			ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+		}
+		else {
+			initializeLogic();
+		}
+	}
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1000) {
+			initializeLogic();
+		}
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		webview1 = (WebView) findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		webview1.loadUrl("https://www.technicalreva.cf/");
+		_download_to("/TECHNICALREVA");
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	public void _download_to (final String _path) {
+		if (FileUtil.isExistFile(_path)) {
+			
+		}
+		else {
+			FileUtil.makeDir(_path);
+		}
+		webview1.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+				String cookies = CookieManager.getInstance().getCookie(url);
+				request.addRequestHeader("cookie", cookies);
+				request.addRequestHeader("User-Agent", userAgent);
+				request.setDescription("Downloading file...");
+				request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+				request.allowScanningByMediaScanner(); request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+				java.io.File aatv = new java.io.File(Environment.getExternalStorageDirectory().getPath() + "/Webview/Download");if(!aatv.exists()){if (!aatv.mkdirs()){ Log.e("TravellerLog ::","Problem creating Image folder");}} request.setDestinationInExternalPublicDir(_path, URLUtil.guessFileName(url, contentDisposition, mimetype));
+				
+				DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+				manager.enqueue(request);
+				showMessage("Downloading File....");
+				BroadcastReceiver onComplete = new BroadcastReceiver() {
+					public void onReceive(Context ctxt, Intent intent) {
+						showMessage("Download Complete!");
+						unregisterReceiver(this);
+					}};
+				registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+			}
+		});
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
